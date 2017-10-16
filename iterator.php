f9b6b734aa0e9a401260c0ed58364a697d2ea16a
wazsmwazsm <18748406022@163.com>
2017-10-16 20:03:49 +0800

    iterator

--- iterator.php
+++ iterator.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+
+    Iterator 接口描述
+
+    Iterator extends Traversable
+    {
+      abstract public mixed current ( void )
+      abstract public scalar key ( void )
+      abstract public void next ( void )
+      abstract public void rewind ( void )
+      abstract public boolean valid ( void )
+    }
+
+*/
+
 class TestIterator implements Iterator
 {
     /*
