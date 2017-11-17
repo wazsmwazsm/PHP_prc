58ed1aeb7fdd56676cf7a7cd2c430ecb2ef84855
wazsmwazsm <18748406022@163.com>
2017-11-17 19:33:54 +0800

    chapter9 stub2

---
+++ phpunit/chapter9/StubTest2.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/SomeClass.php';
+
+use PHPUnit\Framework\TestCase;
+
+class StubTest extends TestCase
+{
+    public function testStub()
+    {
+        // 更详细的创建
+        $stub = $this->getMockBuilder(SomeClass::class)
+                     ->disableOriginalConstructor()
+                     ->disableOriginalClone()
+                     ->disableArgumentCloning()
+                     ->disallowMockingUnknownTypes()
+                     ->getMock();
+
+        // 上桩
+        $stub->method('doSomething')->willReturn('foo');
+
+        // 现在调用 $stub->doSomething() 将返回 'foo'。
+        $this->assertEquals('foo', $stub->doSomething());
+    }
+
+}
