45a68691e4665a9d4c39a9d1ec0b34dfaa504d25
wazsmwazsm <18748406022@163.com>
2017-11-23 19:56:05 +0800

    phpunit chapter9 mocktest6

--- phpunit/chapter9/MockTest6.php
+++ phpunit/chapter9/MockTest6.php
@@ -9,14 +9,17 @@ class FooTest extends TestCase
     // 测试某个方法将会被调用一次，并且以某个特定对象作为参数。
     public function testFunctionCalledTwoTimesWithSpecificArguments()
     {
-
-        $cloneArguments = true;
+        $expectedObject = new stdClass;
 
         $mock = $this->getMockBuilder(stdClass::class)
-                     ->enableArgumentCloning()
-                     ->getMock();
+              ->setMethods(['foo'])
+              ->getMock();
 
-        // 现在仿件将对参数进行克隆，因此 identicalTo 约束将会失败。
+        $mock->expects($this->once())
+             ->method('foo')
+             ->with($this->identicalTo($expectedObject));
+
+        $mock->foo($expectedObject);
     }
 
 }
