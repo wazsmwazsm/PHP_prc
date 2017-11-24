bac3ef2d93c4d8e037f8e13919d92d4f7c2cb0da
wazsmwazsm <18748406022@163.com>
2017-11-24 10:48:50 +0800

    phpunit chapter9 trait test1

---
+++ phpunit/chapter9/TraitTest1.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+trait AbstractTrait
+{
+    public function concreteMethod()
+    {
+        return $this->abstractMethod();
+    }
+
+    public abstract function abstractMethod();
+}
+
+
+class TraitClassTest extends TestCase
+{
+    public function testConcreteMethod()
+    {
+        $mock = $this->getMockForTrait(AbstractTrait::class);
+
+        $mock->expects($this->any())
+             ->method('abstractMethod')
+             ->will($this->returnValue(true));
+
+        $this->assertTrue($mock->concreteMethod());
+    }
+
+}
