559304ffdfac4bea02e3ae911b7eed0e27fc7eef
wazsmwazsm <942443360@qq.com>
2018-12-01 18:08:29 +0800

    add last modified test

---
+++ http/LastModify.php
@@ -0,0 +1,14 @@
+<?php
+
+ini_set('date.timezone', 'PRC');
+
+$date = (int)(time() / 60) * 100;
+
+$contents = date("D, d M Y H:i:s ", $date).'GMT';
+
+if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] = $contents) {
+    header('HTTP/1.1 304 Not Modified');
+} else {
+    header('Last-Modified:'.$contents);
+    echo $contents;
+}
