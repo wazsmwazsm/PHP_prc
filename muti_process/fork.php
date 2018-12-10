31ddaada11c3946a7867e4eee2619812a4d2df93
wazsmwazsm <942443360@qq.com>
2018-12-10 17:50:55 +0800

    modify fork

--- muti_process/fork.php
+++ muti_process/fork.php
@@ -1,17 +1,20 @@
 <?php
 
-$p_pid = posix_getpid();
-
+// fork是创建了一个子进程，父进程和子进程 都从fork的位置开始向下继续执行，
+// 不同的是父进程执行过程中，得到的fork返回值为子进程 号，而子进程得到的是0。
 $pid = pcntl_fork();
 
-if ($pid == -1) {
+if ($pid == -1) { // -1 失败
     throw new Exception('fork faild');
-} else if ($pid > 0) {
-    cli_set_process_title("im parent process {$p_pid}.");
-    pcntl_wait($status);
-} else {
+} else if ($pid > 0) { // 得到子进程号，这里是父进程执行的逻辑
+    // 获取当前进程的进程号
+    $p_pid = posix_getpid(); 
+    cli_set_process_title("im parent process {$p_pid}."); // 设置进程标题
+    pcntl_wait($status); // 等待子进程中断，防止子进程成为僵尸进程。
+} else { // 得到的是 0，这里是子进程执行的逻辑
+    // 获取当前进程的进程号
     $cpid = posix_getpid();
-    cli_set_process_title("im sub process $cpid, my parent is $p_pid");
+    cli_set_process_title("im sub process $cpid");
     while(1);
 }
 
