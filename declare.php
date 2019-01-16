f70e51c65579c48f8711de4075b3479227335c67
wazsmwazsm <942443360@qq.com>
2019-01-16 10:43:16 +0800

    update declare

--- declare.php
+++ declare.php
@@ -1,16 +1,18 @@
 <?php
 // declare(ticks=N);
-// Zend 引擎每执行 N 条低级语句就去执行一次 register_tick_function() 注册的函数
+// Zend 引擎每执行 N 条 nternal statements（opcodes）就去执行一次 register_tick_function() 注册的函数，类似中断机制
+// PHP statements 和 internal statements 并非严格一一对应，所以ticks = N的N，可能不好确定
 declare(ticks=1);
 
 // 检查是否已经超时
 function check_timeout(){
     static $count = 0;
-
+    global $a;
     $count++;
     sleep(1);
+    echo $a;
     if ($count > 10) {
-        exit('time out!');
+        exit("time out!\n");
     }
 }
 
@@ -18,6 +20,7 @@ function check_timeout(){
 register_tick_function('check_timeout');
 
 // 执行语句
-while(1){
-   $a = 1;
+$a = 1; 
+while(1) {
+   ++$a; 
 }
