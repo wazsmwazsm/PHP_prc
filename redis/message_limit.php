482ef32bed4a3f6e7983782ef7674dc512b933b5
wazsmwazsm <942443360@qq.com>
2019-01-27 10:53:10 +0800

    update message limit

--- redis/message_limit.php
+++ redis/message_limit.php
@@ -7,40 +7,40 @@ function is_limited($phone)
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
     
-    $limite_count = 3;
-    $time_limit = 10 * 60; // 10 分钟
+    $now = time();
+    $limite_count = 3; // 不得超过 3 次
+    $time_limit = 600; // 10 分钟限制时段
     $key = 'expires:'. (string) $phone;
 
     // 缓存是否存在
     if ( ! $redis->exists($key)) { 
-        
-        $out_time = time() + $time_limit; // 计算过期时间
-        $value = [$out_time, ];
+        // 保存此次发送的过期时间
+        $value = [$now + $time_limit, ];
         // 设置 redis 过期时间 time_limit， time_limit 时间内无更新的话舍弃掉这个缓存
         $redis->setex($key, $time_limit, serialize($value)); 
 
-        return true;
+        return false;
     } 
 
     // 获取缓存信息
     $value = unserialize($redis->get($key));
     
     // 删除过期数据
-    while (isset($value[0]) && $value[0] <= time()) {
+    while (isset($value[0]) && $value[0] <= $now) {
         array_shift($value);
     }
 
     // 次数是否超限制
     if (count($value) >= $limite_count) {
-        return false;
+        return true;
     }
 
     // 添加新数据
-    $value[] = time() + $time_limit;
+    $value[] = $now + $time_limit;
     // 设置 redis 过期时间 time_limit， time_limit 时间内无更新的话舍弃掉这个缓存
     $redis->setex($key, $time_limit, serialize($value));
 
-    return true;
+    return false;
 }
 
-var_dump(is_limited('1874488449'));
+var_dump(is_limited('18766667777'));
