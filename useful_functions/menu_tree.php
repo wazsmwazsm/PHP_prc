53fcb7dc427d73ef008c1edd1e4b0d3588b1e22a
wazsmwazsm <942443360@qq.com>
2019-02-26 19:08:46 +0800

    add menu tree

---
+++ useful_functions/menu_tree.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$source = [
+    ['id' => 1, 'pid' => 0, 'name' => '动物'],
+    ['id' => 2, 'pid' => 1, 'name' => '软体'],
+    ['id' => 3, 'pid' => 1, 'name' => '哺乳'],
+    ['id' => 4, 'pid' => 0, 'name' => '植物'],
+    ['id' => 5, 'pid' => 2, 'name' => '蜗牛'],
+    ['id' => 6, 'pid' => 3, 'name' => '奶牛'],
+    ['id' => 7, 'pid' => 3, 'name' => '绵羊'],
+    ['id' => 8, 'pid' => 4, 'name' => '菊花'],
+    ['id' => 9, 'pid' => 4, 'name' => '仙人掌'],
+];
+
+function tree($data, $pid)
+{
+    $sub = [];
+    foreach ($data as $key => $value) {
+        if ($value['pid'] == $pid) {
+            $value['sub'] = tree($data, $value['id']);
+            $sub[] = $value;
+        }
+    }
+    return $sub;
+}
+
+var_dump(tree($source, 0));
